e7ab4be6cbcf1cc2ba340429fab8ebe9c6704891
siyamrh7 <siyamrh7@gmail.com>
2026-04-26 14:52:19 +0600

    fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = import.meta.env.VITE_API_URL || '';
 
 const api = axios.create({
-  baseURL: baseURL + '/api',
+  baseURL: baseURL + 'api',
   headers: { 'Content-Type': 'application/json' }
 });
 
